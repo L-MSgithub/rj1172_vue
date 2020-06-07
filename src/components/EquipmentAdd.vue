7b98be6ce80ddcc2567a0a8c19b1375900d8ddfc
782158259 <64392862+782158259@users.noreply.github.com>
2020-06-07 22:11:38 +0800

    修改器材购置新增页面内容

--- src/components/EquipmentAdd.vue
+++ src/components/EquipmentAdd.vue
@@ -2,10 +2,12 @@
     <el-form :inline="true" :model="formInline">
         <el-form-item label="器材名称">
             <el-select v-model="formInline.region" placeholder="器材名称">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
+                <el-option label="羽毛球" value="羽毛球"></el-option>
+                <el-option label="篮球" value="篮球"></el-option>
+                <el-option label="足球" value="足球"></el-option>
+                <el-option label="排球" value="排球"></el-option>
             </el-select>
-            <el-input-number v-model="num" @change="" :min="1" :max="10"></el-input-number>
+            <el-input-number v-model="num" :min="1" :max="10"></el-input-number>
         </el-form-item>
         <el-form-item>
             <el-button type="primary" @click="onSubmit">添加</el-button>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,52 +1,54 @@
 <template>
-    <el-container>
-        <el-header>
-            <el-row type="flex" justify="space-between">
-                <h3>广东海洋大学体育馆管理系统</h3>
-                <el-link :underline="false" style="color: white;"><h3>登录</h3></el-link>
-            </el-row>
-        </el-header>
+    <div style = "background-color: aliceblue">
         <el-container>
-            <el-col :span="4">
-                <el-menu
-                        class="el-menu-vertical-demo"
-                        @open="handleOpen"
-                        @close="handleClose"
-                        background-color="#545c64"
-                        text-color="#fff"
-                        active-text-color="#ffd04b">
-                    <el-submenu index="1">
-                        <template slot="title">
-                            <i class="el-icon-location"></i>
-                            <span>导航一</span>
-                        </template>
-                        <el-menu-item index="1-1">选项1</el-menu-item>
-                        <el-menu-item index="1-2">选项2</el-menu-item>
-                        <el-menu-item index="1-3">选项3</el-menu-item>
-                        <el-menu-item index="1-4">选项4</el-menu-item>
-                    </el-submenu>
-                    <el-menu-item index="2">
-                        <i class="el-icon-menu"></i>
-                        <span slot="title">导航二</span>
-                    </el-menu-item>
-                    <el-menu-item index="3">
-                        <i class="el-icon-document"></i>
-                        <span slot="title">导航三</span>
-                    </el-menu-item>
-                    <el-submenu index="4">
-                        <template slot="title">
-                            <i class="el-icon-setting"></i>
-                            <span slot="title">器材管理</span>
-                        </template>
-                        <el-menu-item v-for="item in $router.options.routes[2].children" :index="item.path"><router-link :to="item.path">{{item.name}}</router-link></el-menu-item>
-                    </el-submenu>
-                </el-menu>
-            </el-col>
-            <el-main>
-                <router-view></router-view>
-            </el-main>
+            <el-header>
+                <el-row type="flex" justify="space-between">
+                    <h3>广东海洋大学体育馆管理系统</h3>
+                    <el-link :underline="false" style="color: white;"><h3>登录</h3></el-link>
+                </el-row>
+            </el-header>
+            <el-container>
+                <el-col :span="4">
+                    <el-menu
+                            class="el-menu-vertical-demo"
+                            @open="handleOpen"
+                            @close="handleClose"
+                            background-color="#545c64"
+                            text-color="#fff"
+                            active-text-color="#ffd04b">
+                        <el-submenu index="1">
+                            <template slot="title">
+                                <i class="el-icon-location"></i>
+                                <span>导航一</span>
+                            </template>
+                            <el-menu-item index="1-1">选项1</el-menu-item>
+                            <el-menu-item index="1-2">选项2</el-menu-item>
+                            <el-menu-item index="1-3">选项3</el-menu-item>
+                            <el-menu-item index="1-4">选项4</el-menu-item>
+                        </el-submenu>
+                        <el-menu-item index="2">
+                            <i class="el-icon-menu"></i>
+                            <span slot="title">导航二</span>
+                        </el-menu-item>
+                        <el-menu-item index="3">
+                            <i class="el-icon-document"></i>
+                            <span slot="title">导航三</span>
+                        </el-menu-item>
+                        <el-submenu index="4">
+                            <template slot="title">
+                                <i class="el-icon-setting"></i>
+                                <span slot="title">器材管理</span>
+                            </template>
+                            <el-menu-item v-for="item in $router.options.routes[2].children" :index="item.path"><router-link :to="item.path">{{item.name}}</router-link></el-menu-item>
+                        </el-submenu>
+                    </el-menu>
+                </el-col>
+                <el-main>
+                    <router-view></router-view>
+                </el-main>
+            </el-container>
         </el-container>
-    </el-container>
+    </div>
 </template>
 
 <script>
@@ -80,7 +82,6 @@
     }
 
     .el-main{
-        background-color: aliceblue;
         border-radius: 5px;
         height: 600px;
     }
